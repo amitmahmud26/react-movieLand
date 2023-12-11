9c77582c2dc03e24c0686591eaf4fde8b582262f
Amit Mahmud Sabbir <amitsclan26@gmail.com>
2023-12-11 11:50:01 +0600

    first commit

---
+++ crash-course/src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import './App.css';
+
+// 547433ca
+
+const API_URL = 'https://www.omdbapi.com/?apikey=547433ca'
+const App = () => {
+  return (
+    <p>app</p>
+  );
+}
+
+export default App;
+ 
